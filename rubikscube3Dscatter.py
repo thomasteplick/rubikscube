1874c7f72ae0a4755161b6676d234a3220cd2119
Thomas Teplick <117768679+thomasteplick@users.noreply.github.com>
2026-09-06 10:36:30 -0700

    Add files via upload

---
+++ rubikscube3Dscatter.py
@@ -0,0 +1,69 @@
+"""
+==============
+3D scatterplot
+==============
+
+Demonstration of a basic scatterplot in 3D.
+"""
+
+import matplotlib.pyplot as plt
+import argparse
+
+# size of the container for the geometric object
+dim = 48
+
+# get file to plot the from command line
+parser = argparse.ArgumentParser(prog='scatter3D', description="3D Scatterplot of Rubik's cube")
+parser.add_argument('io', type=int, choices=[0,1])
+args = parser.parse_args()
+
+# scrambled cube, solved cube
+file = ["rubik_scrambled.txt", "rubik_solved.txt"]
+state = ["scrambled", "solved"]
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+'''
+	std::vector<int> density2FGcolor = {
+		FOREGROUND_YELLOW,
+		FOREGROUND_GRAY,
+		FOREGROUND_ORANGE,
+		FOREGROUND_CYAN,
+		FOREGROUND_GREEN,
+		FOREGROUND_BLUE,
+		FOREGROUND_RED,
+		FOREGROUND_PURPLE,
+		FOREGROUND_BROWN,
+		FOREGROUND_WHITE,
+	};
+'b' as blue
+'g' as green
+'r' as red
+'c' as cyan
+'m' as magenta
+'y' as yellow
+'k' as black
+'w' as white
+'''
+
+# Define colors
+clrs = ['w', 'w', 'y', 'r', 'tab:orange', 'g', 'b', 'k']
+mkr = '.'
+# Read in cube file into geometric 3D np array
+with open(file[args.io], mode='r', encoding="utf-8") as f:
+    for i in range(dim):
+        for j in range(dim):
+            line = f.readline()
+            item = line.split()
+            for k, val in enumerate(item):
+                ival = int(val)
+                if (ival > 0):
+                    #ax.scatter(np.array(i), np.array(j), np.array(k), marker=mkr, c=clrs[ival])
+                    ax.scatter(i, j, k, marker=mkr, c=clrs[ival])
+                        
+ax.set_xlabel('Front face')
+ax.set_ylabel('Right face')
+ax.set_zlabel('Up face')
+plt.title("3D Scatterplot of Rubik's Cube, %s" % state[args.io])
+plt.show()
